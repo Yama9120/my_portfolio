61a7e1af1c284281575721b0842b091eef38a4b9
yama9120 <117654023+Yama9120@users.noreply.github.com>
2025-03-06 03:57:00 +0900

    feat: project detail

--- app/lib/data/projects.ts
+++ app/lib/data/projects.ts
@@ -25,7 +25,7 @@ export const projects: Project[] = [
             "現在はログイン時のみ利用可能な便利機能を随時追加する形で運営しております。"
         ].join('\n'),
         imageUrl: "images/projects/librarin_top.png",
-        technologies: ["node.js", "JavaScript", "EJS", "CSS", "firebase", "docker", "nginx"],
+        technologies: ["node.js", "JavaScript", "EJS", "CSS", "Firebase", "Docker", "Nginx"],
         githubUrl: "https://github.com/yourusername/todo-app",
         serviceUrl: "https://librarin.tgif.hiroshima-u.ac.jp/"
     },
@@ -54,7 +54,7 @@ export const projects: Project[] = [
             "ヒアリングと制作を繰り返し、理想の形を作りあげられるように努力しています。",
         ].join('\n'),
         imageUrl: "https://placehold.co/600x400", // ダミー画像を使用
-        technologies: ["Illustrator", "Photoshop", "Clip Studio Paint"],
+        technologies: ["Illustrator", "Photoshop", "Clip Studio Paint", "Blender"],
         githubUrl: "https://github.com/yourusername/todo-app"
     },
     {
@@ -68,7 +68,7 @@ export const projects: Project[] = [
             "また、このサイトでは、今までの開発の知識を活かし、効率的な開発を心がけております。具体的には、技術スタックやプロジェクトの詳細を表示するためのコンポーネントを作成し、再利用することで、効率的にコンテンツを追加できるようにしています。"
         ].join('\n'),
         imageUrl: "https://placehold.co/600x400", // ダミー画像を使用
-        technologies: ["Figma", "Next.js", "TypeScript", "MUI"],
+        technologies: ["Figma", "Next.js", "TypeScript", "Mui"],
         githubUrl: "https://github.com/yourusername/portfolio",
         serviceUrl: "https://your-portfolio.vercel.app"
     },
@@ -86,5 +86,20 @@ export const projects: Project[] = [
         technologies: ["Python", "Pyxel", "Clip Studio Paint"],
         githubUrl: "https://github.com/yourusername/todo-app",
         serviceUrl: "https://your-portfolio.vercel.app"
-    }
+    },
+    {
+        id: 6,
+        title: "Youtubeチャット拡張機能",
+        description: "Youtubeのチャット欄を好みの位置に表示するChrome拡張機能",
+        detailedDescription: [
+            "Youtubeのチャット欄を好みの位置に表示するChrome拡張機能です。",
+            "Youtubeのチャット欄は、通常全画面表示ではない場合に右側や下側に表示されますが、この拡張機能を使用することで、全画面表示時に、四隅の何処か・ポップアップとして表示することができます。",
+            "この拡張機能を作成することで、Chrome拡張機能の作り方を学びました。",
+            "また、この拡張機能を作成することで、不便だと感じる部分を自分で解決することができるということを学びました。"
+        ].join('\n'),
+        imageUrl: "https://placehold.co/600x400", // ダミー画像を使用
+        technologies: ["TypeScript"],
+        githubUrl: "https://github.com/yourusername/todo-app",
+        serviceUrl: "https://your-portfolio.vercel.app"
+    },
 ];
